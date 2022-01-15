7b66c325cd497c28c75b3e74c5ea5199abe2410c
viqueen <hasnae@viqueen.org>
2022-01-15 22:17:57 +1100

    extract child pages

--- src/generator/extract-page-tree.ts
+++ src/generator/extract-page-tree.ts
@@ -7,9 +7,12 @@ const extractPageTree = async (
     outputDirectories: OutputDirectories,
     asHomepage = false
 ) => {
-    console.info('▶️ extract page tree: ', id);
+    console.info('▶️  extract page tree: ', id);
     const content = await api.getContent(id, asHomepage);
     await extractContent(content, outputDirectories);
+    for (const child of content.children) {
+        await extractPageTree(child.id, outputDirectories);
+    }
 };
 
 export default extractPageTree;
