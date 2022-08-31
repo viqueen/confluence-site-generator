e422386834d717dd95c92168fe7020431c2dc925
Hasnae <hasnae@viqueen.org>
2022-08-31 17:24:59 +1000

    welcome emoji provider

--- src/site/content/providers/index.tsx
+++ src/site/content/providers/index.tsx
@@ -1,7 +1,31 @@
 import { ProviderFactory } from '@atlaskit/editor-common/provider-factory';
+import { EmojiId, EmojiProvider } from '@atlaskit/emoji';
+import { OptionalEmojiDescriptionWithVariations } from '@atlaskit/emoji/types';
+
+const emojiProvider = async () => {
+    const fetchByEmojiId = async (
+        emojiId: EmojiId
+    ): Promise<OptionalEmojiDescriptionWithVariations> => {
+        return {
+            ...emojiId,
+            type: 'static',
+            category: 'static',
+            searchable: false,
+            representation: {
+                imagePath: `/assets/emojis/${emojiId.id}.png`,
+                width: 64,
+                height: 64
+            }
+        };
+    };
+    const fetchEmojiProvider = async () => undefined;
+    return { fetchByEmojiId, fetchEmojiProvider } as unknown as EmojiProvider;
+};
 
 const dataProviders = () => {
-    return ProviderFactory.create({});
+    return ProviderFactory.create({
+        emojiProvider: emojiProvider()
+    });
 };
 
 export { dataProviders };
